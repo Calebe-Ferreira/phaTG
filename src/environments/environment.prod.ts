6ca1aefa8034d0bac2d063a1e12fc6dccec529e7
Calebe Ferreira <calebe-ferreira@hotmail.com>
2018-11-01 23:09:15 -0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  
+  firebaseConfig: {
+    //firebase config
+    apiKey: "AIzaSyBl_YQXa6AayDrx1zGZz2aL1HAM6UJ5sTI",
+    authDomain: "projeto-tg-pha.firebaseapp.com",
+    databaseURL:"https://projeto-tg-pha.firebaseio.com",
+    projectId: "projeto-tg-pha",
+    storageBucket: "projeto-tg-pha.appspot.com",
+    messagingSenderId: "35170508886"
+  },
+    googleMapsKey:'AIzaSyBUHvhdcg6ojS4dsIG8rWKPnP1F6eilXVA'
 };
